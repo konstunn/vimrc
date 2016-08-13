7a133d3c630c7cecdf9e5064cf6eb6b8b335b7f3
Konstantin Gorbunov <konstunn@users.noreply.github.com>
2016-08-13 22:30:51 +0700

    Install: check if dest branch already exists

--- .install.sh
+++ .install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 SRC_REPO_NAME='konstunn_dotfiles_local'
+DEST_BRANCH='konstunn_dotfiles_installation'
 
 cd ~/
 
 GIT_REPO_EXISTS=0
 
-if [ -d .git ] ; then
+if [ -d .git ]
+then
 	git stash
 	GIT_REPO_EXISTS=1
 else
@@ -22,11 +24,17 @@ git reset .gitignore
 rm .gitignore
 git commit -m 'backup existed files'
 
-if [ $GIT_REPO_EXISTS -eq 1 ] ; then
-	git checkout --orphan konstunn_dotfiles
-	git merge $SRC_REPO_NAME
-else
-	git checkout master
+if [ $GIT_REPO_EXISTS -eq 1 ]
+then
+	BRANCH_EXISTS=`git branch | grep " $DEST_BRANCH$" | wc -l`
+	if [ $BRANCH_EXISTS -eq 0 ]
+	then
+		git checkout --orphan $DEST_BRANCH
+	else
+		git checkout $DEST_BRANCH
+	fi
 fi
+
+git merge --no-edit -X 'theirs' $SRC_REPO_NAME/master
 
 cd $OLDPWD
